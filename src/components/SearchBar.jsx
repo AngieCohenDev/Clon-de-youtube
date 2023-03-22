b4ccb72f12b924b40e364c99a75c0fcecf3fae38
Angie Cohen <angiedev2001@gmail.com>
2023-03-22 15:43:24 -0300

    Se creo la SearchBar y se la agregaron funciones

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
+import { Search } from "@mui/icons-material";
+import { red } from "@mui/material/colors";
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
-  )
-}
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value=""
+        onClick={() => {}}
+      />
+      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
