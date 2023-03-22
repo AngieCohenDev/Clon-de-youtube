406c8d6c79b4bcbe779fc826384ea0acc62174be
Angie Cohen <angiedev2001@gmail.com>
2023-03-22 18:01:41 -0300

    ahora la barra de navegacion cambia de color a rojo cuando ha sido seleccionada

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,15 @@
 import { useState, useEffect } from "react";
-import { Box, Stack, Typography,  } from "@mui/material";
-import SideBar from "./SideBar";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { fetchfromAPI } from "../utils/fecthFromAPI";
+import { SideBar, Videos } from "./";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  useEffect(() => {
+    fetchfromAPI(`search?part=snippet&q=${selectedCategory}`);
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -12,7 +19,7 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <SideBar></SideBar>
+        <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         <Typography
           className="copyright"
           variant="body2"
@@ -20,6 +27,18 @@ const Feed = () => {
         >
           Copyrigth 2022 Angie Cohen
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
